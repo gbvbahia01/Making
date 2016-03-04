4d6328e5bb69fd16e393c5370f68ca3efc7d7202
Guilherme Braga <gbvbahia01@gmail.com>
2016-03-04 17:13:40 -0300

    Id JPA is null by default in Make

---
+++ src/main/java/br/com/gbvbahia/making/entity/jsr303/Employer.java
@@ -0,0 +1,10 @@
+package br.com.gbvbahia.making.entity.jsr303;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYER_TB")
+public class Employer {
+
+}
--- src/test/java/br/com/gbvbahia/making/dao/EmployeeDaoTest.java
+++ src/test/java/br/com/gbvbahia/making/dao/EmployeeDaoTest.java
@@ -1,7 +1,6 @@
 package br.com.gbvbahia.making.dao;
 
 import br.com.gbvbahia.maker.MakeEntity;
-import br.com.gbvbahia.making.dao.EmployeeDao;
 import br.com.gbvbahia.making.entity.jsr303.Employee;
 
 import org.junit.After;
@@ -10,26 +9,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class EmployeeDaoTest {
-   private EmployeeDao employeeDao = null;
+  private EmployeeDao employeeDao = null;
 
-   @Before
-   public void setup() throws Exception {
-      this.employeeDao = new EmployeeDao();
-      this.employeeDao.getEntityManager();
-   }
+  @Before
+  public void setup() throws Exception {
+    this.employeeDao = new EmployeeDao();
+    this.employeeDao.getEntityManager();
+  }
 
-   @After
-   public void clear() {
-      this.employeeDao.getEntityManager().close();
-   }
+  @After
+  public void clear() {
+    this.employeeDao.getEntityManager().close();
+  }
 
-   @Test
-   public void testCreateEmployee() throws Exception {
-      Employee employee = MakeEntity.make(Employee.class);
-      employee.setId(null);
-      this.employeeDao.create(employee);
-      Assert.assertFalse("The emplyee list cannot be empty", this.employeeDao
-            .findAll().isEmpty());
-   }
+  @Test
+  public void testCreateEmployee() throws Exception {
+    Employee employee = MakeEntity.make(Employee.class);
+    this.employeeDao.create(employee);
+    Assert.assertFalse("The emplyee list cannot be empty", this.employeeDao
+        .findAll().isEmpty());
+  }
 
 }
